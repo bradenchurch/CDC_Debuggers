c76c09430177feaa4d461be99f3dd46ad2cd0f31
Braden Church <bradenchurch@gmail.com>
2020-07-11 12:15:41 -0600

    auth updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route } from 'react-router-dom';
+// import Home from './components/home/Home';
+import Login from './auth/Login';
+import Register from './auth/Register';
+import Navbar from './shared/Navbar';
+// import NoMatch from './components/shared/NoMatch';
+import FetchUser from './auth/FetchUser';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import ProtectedRoute from './auth/ProtectedRoute';
 
+
+
+const App = () => (
+  <>
+    <Navbar />
+    <FetchUser>
+      <Switch>
+        {/* <Route exact path='/' component={Home} /> */}
+        {/* <Route exact path='/Feed' component={Feed} /> */}
+        {/* <ProtectedRoute exact path='/profile' component={Profile} /> */}
+        {/* <ProtectedRoute exact path='/collection' component={Collection} /> */}
+        {/* <ProtectedRoute exact path='/dash' component={Dash} /> */}
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/register' component={Register} />
+
+        {/* <Route component={NoMatch} /> */}
+      </Switch>
+    </FetchUser>
+  </>
+)
 export default App;
