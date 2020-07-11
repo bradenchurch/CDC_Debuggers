7e7358d3741d0453c0a459b8fbf07131694b21e0
jacob koskimaki <jkoskimaki@gmail.com>
2020-07-11 12:50:08 -0600

    home_page

---
+++ client/src/components/home/Home.js
@@ -0,0 +1,25 @@
+import React from "react";
+import styled from "styled-components";
+
+const Home = () => (
+  <>
+  <h1>CDC Guidline Compliance</h1>
+  <h2>Nursing Home Safety</h2>
+    <Hero>
+    </Hero>
+  </>
+);
+
+const Hero = styled.div`
+  width: 100%;
+  height: 70%;
+  left: 0px;
+  background: url("https://www.easttexasmatters.com/wp-content/uploads/sites/34/2020/06/cdc-covid-banner-1.jpg?w=640&h=360&crop=1");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: absolute;
+  margin: 30px;
+`
+
+export default Home;
