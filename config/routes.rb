6b5e6feef420c7fe2db4ef330991b129c2409c35
Braden Church <bradenchurch@gmail.com>
2020-07-11 16:28:18 -0600

    facility quesionstions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+
+  mount_devise_token_auth_for 'User', at: 'api/auth'
+  
   namespace :api do
     resources :users
+
     resources :facility_data
     resources :facility_inspection
     resources :guidelines
